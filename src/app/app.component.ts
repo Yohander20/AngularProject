90cf124a540eb41ebf549eac99ef9eda3b868dbf
Sistemas GIP <sistemas@polybarq.com>
2022-08-10 15:16:56 -0400

    subir archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,91 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+
 })
 export class AppComponent {
-  title = 'my-store';
+  widthImg=10;
+  name='Nicolas';
+  age=18;
+  imag='https://electronicssoftware.net/wp-content/uploads/user.png';
+  btnDisable=true;
+  register={
+    name:'',
+    email:'',
+    password: ''
+  }
+  person={
+    name:'Nicolas',
+    age:18,
+    avatar:'https://electronicssoftware.net/wp-content/uploads/user.png'
+  }
+  toggleButton(){
+    this.btnDisable=!this.btnDisable;
+  }
+
+  increaseAge(){
+   this.person.age+=1;
+  }
+
+  decretAge(){
+    this.person.age-=1;
+  }
+
+  onScroll(event:Event){
+    const element=event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  names: string[]=['Nico','Juli','Santi'];
+  newName='';
+  box={
+    width:100,
+    height:100,
+    background:'red'
+  };
+  products=[
+    {
+      name:'manzana',
+      price:565,
+      image:'./assets/images/manzana.jpg',
+
+    },
+    {
+      name:'maiz',
+      price:600,
+      image:'./assets/images/maiz.jpg'
+    },
+    {
+      name:'huevos',
+      price:200,
+      image:'./assets/images/huevos.jpeg'
+    },
+    {
+      name:'pescado',
+      price:300,
+      image:'./assets/images/pescado.jpg'
+    },
+    {
+      name:'leche',
+      price:300,
+      image:'./assets/images/tipos-leche.jpg'
+    }
+  ]
+
+  addName(){
+    this.names.push(this.newName);
+    this.newName='';
+  }
+
+  deleteName(index:number){
+    this.names.splice(index,1);
+  }
+
+  onRegister(){
+    console.log(this.register);
+  }
+
+
 }
+
+
---
+++ src/app/recap.ts
@@ -0,0 +1,6 @@
+const username = 'Yohander';
+
+const sum = (a:number,b:number) => {
+  return a + b;
+}
+sum(1,3);
